5ecfca3ec59e942b153428e0411ea71b61c4f511
Carlo de Wolf <cdewolf@redhat.com>
2010-05-07 19:52:37 +0200

    Adding a double parent test

--- src/test/java/org/jboss/beach/util/collection/test/SimpleTestCase.java
+++ src/test/java/org/jboss/beach/util/collection/test/SimpleTestCase.java
@@ -3,6 +3,7 @@ package org.jboss.beach.util.collection.test;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
@@ -26,5 +27,30 @@ public class SimpleTestCase
       }
 
       assertEquals(0, parent.getChildren().size());
+   }
+
+   @Test
+   public void testTwoParents()
+   {
+      MockParent parent1 = new MockParent();
+
+      MockParent parent2 = new MockParent();
+
+      MockChild child = new MockChild();
+
+      boolean success = parent1.getChildren().add(child);
+      assertTrue(success);
+
+      try
+      {
+         parent2.getChildren().add(child);
+         fail("Expected RuntimeException");
+      }
+      catch(RuntimeException e)
+      {
+         // expected
+      }
+
+      assertEquals(0, parent2.getChildren().size());
    }
 }
